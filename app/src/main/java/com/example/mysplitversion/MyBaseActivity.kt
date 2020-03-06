06756c214c243cf8b086eacfb7f5a43d47e1cf53
anaa pengkhun <se56160422@gmail.com>
2020-03-06 14:49:28 +0700

    init

--- app/src/main/java/com/example/mysplitversion/MyBaseActivity.kt
+++ app/src/main/java/com/example/mysplitversion/MyBaseActivity.kt
@@ -3,4 +3,8 @@ package com.example.mysplitversion
 import androidx.appcompat.app.AppCompatActivity
 
 open class MyBaseActivity : AppCompatActivity() {
+
+    fun showDialog(){
+        println("MyBaseActivity : showDialog")
+    }
 }
