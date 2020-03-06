01c34104c8251598d5fc16b9b335ba54af730748
anaa pengkhun <se56160422@gmail.com>
2020-03-06 16:33:53 +0700

    init

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -2,4 +2,4 @@
 
 echo "++++++++++++ current user+++++++++"
 #echo $(curl -X GET "https://api.appcenter.ms/v0.1/user" -H "accept: application/json" -H "X-API-Token:17543bbbdabcb56cf5b1e77238a493e81ec3fabb")
-echo $APPCENTER_TRIGGER
+echo $APP_DISPLAY_NAME
