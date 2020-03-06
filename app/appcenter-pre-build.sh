b5bc7595959527ce7569e745f2ff2d110f22b184
anaa pengkhun <se56160422@gmail.com>
2020-03-06 16:06:13 +0700

    init

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-
+echo "++++++++++++ current user+++++++++"
 #echo $(curl -X GET "https://api.appcenter.ms/v0.1/user" -H "accept: application/json" -H "X-API-Token:17543bbbdabcb56cf5b1e77238a493e81ec3fabb")
-echo $APPCENTER_OWNER
+echo $ENVIRONMENT_VARIABLE
